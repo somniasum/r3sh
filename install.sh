f093e0650404c2299c3418cf787c65394293f41c
Zv74D <yourdaddiesdick@protonmail.com>
2023-07-27 23:04:16 +0200

    update 2

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#author: Zv74D
+#
+
+source animate.sh
+
 clear
 chmod +x ./*/*.sh
 
@@ -7,7 +12,8 @@ bash ./intro.sh
 
 #STAGE 1
 bash ./stage-1.sh
-echo "stage 1 complete"
+clear
+animate 
 
 #STAGE 2
 bash ./stage-2.sh
