9ed3cfa515e1a7190bbe08a4114a0db007727c82
somniasum <somniasum@proton.me>
2024-08-09 22:04:21 +0200

    updated nvim

--- Dotfiles/nvim/init.lua
+++ Dotfiles/nvim/init.lua
@@ -34,8 +34,19 @@ keymap('n', '<Leader>ps', ':PackerSync<CR>', opts)
 --For PackerUpdate
 keymap('n', '<Leader>pu', ':PackerUpdate<CR>', opts)
 
+--Windows
+keymap("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+keymap("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+keymap("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 
+--Window splits
+keymap('n', '<Leader>v', ':vsp<CR>', opts)
+keymap('n', '<Leader>h', ':sp<CR>', opts)
 
+--Git
+keymap("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+keymap("n", "<leader>gs", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
 --To call all packages
 require('packages')
 require('mason-lsp')
