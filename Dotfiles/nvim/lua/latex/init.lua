712f0f1f228f86254c353e3ae4094686850267a7
somniasum <somniasum@proton.me>
2024-08-19 13:28:38 +0200

    removed nvim dotfiles and added btop

--- Dotfiles/nvim/lua/latex/init.lua
+++
@@ -1,8 +0,0 @@
--- VimTeX configuration
-vim.g.tex_flavor = 'latex'
-vim.g.vimtex_view_method = 'zathura'  -- or 'evince' if you prefer Evince
-vim.g.vimtex_compiler_method = 'latexmk'
-
--- Optional: Enable conceal feature for a cleaner look
-vim.g.tex_conceal = 'abdmg'
-vim.g.vimtex_quickfix_mode = 0
