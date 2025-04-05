3c779c4c9e2408cf86e929ffb45d2b65a8e1dd81
somniasum <somniasum@gmail.com>
2025-04-06 00:38:43 +0200

    added ghostty

--- Dotfiles/fish/config.fish
+++ Dotfiles/fish/config.fish
@@ -1,6 +1,6 @@
-alias config="~/.config/i3/.config.sh"
-
+alias on="/usr/local/bin/backlight.sh on"
+alias off="/usr/local/bin/backlight.sh off"
+alias pass="~/.config/fish/password_grep.sh"
+alias c="cargo r"
+alias b="cargo b"
 starship init fish | source
-
-set -U fish_greeting ""
-
